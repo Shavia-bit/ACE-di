2017acd1302f42652bcbb7d3f38f34ad50ee4ac3
Shavia Kwoma <shavia@shavia.s.org>
2023-01-12 12:20:06 +0300

    ACE-Di

---
+++ forms/contact.php
@@ -0,0 +1,71 @@
+<?php
+
+  $receiving_email_address = 'jmkeitany@gmail.com';
+
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+
+  // }
+
+$errors = '';
+$myemail = 'jmkeitany@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['subject']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if(empty($errors)){
+
+  $to = $myemail;
+  $email_message = "Contact from :$name";
+  $subject = "New message from your site! Email from: $name".
+  "Email Subject: $email_address\n Name: $name\n $message";
+
+  $headers  = "From $myemail\n";
+  $headers .="Reply to: $email_address";
+
+  mail($to,$email_message,$subject,$headers);
+
+
+
+}
+
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
+  
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
+
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // /*
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
+  // */
+
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
+
+  // echo $contact->send();
+?>
